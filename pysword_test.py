dd38d9b39361ef6a2990fa7964ab192715f27a70
Staff Silence <b0cspa@cock.li>
2019-06-04 14:12:17 -0500

    a bit added to heathen

--- pysword_test.py
+++ pysword_test.py
@@ -1,30 +1,38 @@
+from pysword.modules import SwordModules
+import pysword
 import pysword_repo
+import os
 
-repodog = pysword_repo.PyswordRepo()
-repodog.initiate_repo()
-repodog.update_repo_list()
-repodog.download_repos()
-repodog.install_module('kjv.conf-kjv')
-if repodog.uninstall_module('kjv.conf-kjv_CUSTOMPATH'):
-	print("uninstalled")
-repodog.install_module('drc.conf-drc')
-if repodog.uninstall_module('drc.conf-drc_CUSTOMPATH'):
-	print("uninstalled")
-repodog.install_module('abu.conf-abu-beta')
-if repodog.uninstall_module('abu.conf-abu-beta'):
-	print("uninstalled")
-print(repodog.find_module('abu', beta=True))
-print(repodog.find_module('drc'))
-print(repodog.find_module('netfree',outdated=True))
-repodog.install_module('netfree.conf-netfree', outdated=True)
-if repodog.uninstall_module('netfree.conf-netfree'):
-	print("uninstalled")
-print(repodog.search_section_module("king james","about"))
+repodog = pysword_repo.PyswordRepo(swrdpath = os.path.join(os.getenv(u'HOME'), u'.sword'))
+#repodog.initiate_repo()
+#repodog.update_repo_list()
+#repodog.download_repos()
+#repodog.install_module('kjv.conf-kjv')
+# if repodog.uninstall_module('kjv.conf-kjv_CUSTOMPATH'):
+	# print("uninstalled")
+# repodog.install_module('drc.conf-drc')
+# if repodog.uninstall_module('drc.conf-drc_CUSTOMPATH'):
+	# print("uninstalled")
+# repodog.install_module('abu.conf-abu-beta')
+# if repodog.uninstall_module('abu.conf-abu-beta'):
+	# print("uninstalled")
+# print(repodog.find_module('abu', beta=True))
+# print(repodog.find_module('drc'))
+# print(repodog.find_module('netfree',outdated=True))
+# repodog.install_module('netfree.conf-netfree', outdated=True)
+# if repodog.uninstall_module('netfree.conf-netfree'):
+	# print("uninstalled")
+# print(repodog.search_section_module("king james","about"))
 
-print(repodog.find_module('abu'))
-repodog.install_module('chrysostom.conf-chrysostom', preferzip=False)
-repodog.install_module('drc.conf-drc')
-repodog.install_module('kjv.conf-kjv')
-print(repodog.find_module('abu', beta=True))
-print(repodog.find_module('drc'))
-print(repodog.find_module('netfree',outdated=True))
+# print(repodog.find_module('abu'))
+#repodog.install_module('chrysostom.conf-chrysostom', preferzip=False)
+#repodog.uninstall_module('drc.conf-drc')
+#repodog.install_module('drc.conf-drc',preferzip=False)
+# repodog.install_module('kjv.conf-kjv')
+# print(repodog.find_module('abu', beta=True))
+# print(repodog.find_module('drc'))
+# print(repodog.find_module('netfree',outdated=True))
+modules = SwordModules()
+bible = modules.get_bible_from_module(u'd')
+print(bible)
+#print(bible.get(books=['john'], chapters=[3], verses=16))
